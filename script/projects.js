5fa45d0a854448718660bfed38c4147ee49ace64
Anthony DePaul <anthony@adepaul.dev>
2022-08-20 18:41:13 -0400

    Projects page & tweaks - Added projects page w/ filter functionality - Changed image loading to lazy - Minor tweaks

---
+++ script/projects.js
@@ -0,0 +1,62 @@
+const tagButtons = document.querySelectorAll('#tags .btn')
+const searchInput = document.querySelector('#search input')
+const projects = Array.from(document.querySelectorAll('#projects .project'))
+    .map(elem => {
+        return {
+            "title": elem.querySelector('h4').innerText,
+            "tags": elem.dataset.tags.split(',').map(tag => tag.toLowerCase()),
+            "elem": elem
+        }
+    })
+
+// reverse project columns based on last character of project title
+projects.forEach(proj => {
+    const charCode = proj.title.charCodeAt(proj.title.length - 1)
+
+    const columns = proj.elem.querySelector('.project-content > .columns')
+    if (charCode % 2 === 0) columns.classList.add('reversed')
+})
+
+// tag buttons
+tagButtons.forEach(btn => {
+
+    btn.addEventListener('click', () => {
+
+        if (btn.classList.contains('require')) {
+            btn.classList.remove('require')
+            btn.classList.add('exclude')
+        } else if (btn.classList.contains('exclude')) {
+            btn.classList.remove('exclude')
+        } else {
+            btn.classList.add('require')
+        }
+
+        filterProjects()
+    })
+})
+
+searchInput.addEventListener('input', filterProjects)
+
+// show or hide projects according to tag buttons & search input
+function filterProjects() {
+
+    projects.forEach(proj => {
+
+        proj.elem.classList.remove('disabled')
+
+        // tags filter
+        tagButtons.forEach(button => {
+            const buttonText = button.innerText.toLowerCase()
+            if (button.classList.contains('require') && !proj.tags.includes(buttonText)) {
+                proj.elem.classList.add('disabled')
+            } else if (button.classList.contains('exclude') && proj.tags.includes(buttonText)) {
+                proj.elem.classList.add('disabled')
+            }
+        })
+
+        // search filter
+        if (!proj.title.toLowerCase().includes(searchInput.value.toLowerCase())) {
+            proj.elem.classList.add('disabled')
+        }
+    })
+}
