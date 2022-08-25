1fea4012f02b5174703cc3d335562f7d7eff4ead
Anthony DePaul <anthony@adepaul.dev>
2022-08-24 20:34:55 -0400

    Added example project page & tweaks - Added example project page w/ styling - Added code block formatting fix to common.js - Switched monospaced font - Tweaked common anchor & button styling

--- script/common.js
+++ script/common.js
@@ -1,4 +1,31 @@
 const navbarMenuBtn = document.querySelector('#navbar-menu button')
 const navbarMenuList = document.querySelector('#navbar-menu ul')
+const codeBlocks = document.querySelectorAll('pre code')
 
+// navbar menu button action
 navbarMenuBtn.addEventListener('click', () => navbarMenuList.classList.toggle('disabled'))
+
+// code block formatting
+codeBlocks.forEach(code => {
+    var lines = code.textContent.split('\n')
+
+    // remove leading empty lines
+    while (lines.length != 0 && lines[0].trim().length === 0) {
+        lines.shift()
+    }
+
+    // remove trailing empty lines
+    while (lines.length != 0 && lines[lines.length - 1].trim().length === 0) {
+        lines.pop()
+    }
+    
+    if (lines.length == 0) return
+
+    // count first line's leading spaces
+    var prefixSize = 0
+    while (lines[0].charAt(prefixSize) === ' ') {
+        prefixSize++
+    }
+
+    code.textContent = lines.map(line => line.substring(prefixSize)).join('\n')
+})
